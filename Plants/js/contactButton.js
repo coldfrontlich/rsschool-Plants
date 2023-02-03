1a225160b6278c5eeeb7e8390f4de4a3029ff3e4
coldfrontlich <coldfrontlich@gmail.com>
2023-02-04 02:43:45 +0300

    feat: update contacts dropdown menu and cards

--- Plants/js/contactButton.js
+++ Plants/js/contactButton.js
@@ -10,15 +10,56 @@ document.querySelector('.select-city').addEventListener('click' , (e) => {
       changeCityColor(e.target.parentElement);
     }
   } else if (isDropdown) {
-    if (e.target.firstElementChild.classList.contains('select-arrow_unactive')) {
-      openMenuByArrow(e.target.firstElementChild);
+    if (e.target.firstElementChild.nextElementSibling.classList.contains('select-arrow_unactive')) {
+      openMenuByArrow(e.target.firstElementChild.nextElementSibling);
       changeCityColor(e.target);
     } else {
-      closeMenuByArrow(e.target.firstElementChild);
+      closeMenuByArrow(e.target.firstElementChild.nextElementSibling);
       changeCityColor(e.target);
     }
-  } else {
-    console.log(e.target);
+  }
+})
+
+document.querySelector('.contact-list').addEventListener('click', (e) => {
+  const isCanandaiguaItem = e.target.matches('[data-Canandaigua-item]');
+  const isNewYorkItem = e.target.matches('[data-NewYork-item]');
+  const isYonkersItem = e.target.matches('[data-Yonkers-item]');
+  const isSherrillItem = e.target.matches('[data-Sherrill-item]');
+  if (isCanandaiguaItem) {
+    changeMenuText(e.target);
+    removeAllListElements();
+    changeListElement(e.target);
+    closeMenuByArrow(e.target.parentElement.parentElement.previousElementSibling);
+    changeCityColor(e.target.parentElement.parentElement.parentElement);
+    closeAllCards();
+    openCard(e.target.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling);
+  }
+  if (isNewYorkItem) {
+    changeMenuText(e.target);
+    removeAllListElements();
+    changeListElement(e.target);
+    closeMenuByArrow(e.target.parentElement.parentElement.previousElementSibling);
+    changeCityColor(e.target.parentElement.parentElement.parentElement);
+    closeAllCards();
+    openCard(e.target.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling.nextElementSibling);
+  }
+  if (isYonkersItem) {
+    changeMenuText(e.target);
+    removeAllListElements();
+    changeListElement(e.target);
+    closeMenuByArrow(e.target.parentElement.parentElement.previousElementSibling);
+    changeCityColor(e.target.parentElement.parentElement.parentElement);
+    closeAllCards();
+    openCard(e.target.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling.nextElementSibling.nextElementSibling);
+  }
+  if (isSherrillItem) {
+    changeMenuText(e.target);
+    removeAllListElements();
+    changeListElement(e.target);
+    closeMenuByArrow(e.target.parentElement.parentElement.previousElementSibling);
+    changeCityColor(e.target.parentElement.parentElement.parentElement);
+    closeAllCards();
+    openCard(e.target.parentElement.parentElement.parentElement.parentElement.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling);
   }
 })
 
@@ -44,4 +85,42 @@ const changeCityColor = (city) => {
     city.classList.add('select-city_unactive');
     city.classList.remove('select-city_active');
   }
+}
+
+const changeMenuText = (textMain) => {
+  textMain.parentElement.parentElement.parentElement.firstElementChild.textContent = textMain.textContent;
+}
+
+const removeAllListElements = () => {
+  document.querySelectorAll('.contact-list-item').forEach(listElement => {
+    listElement.classList.add('contact-list-item_unactive');
+    listElement.classList.remove('contact-list-item_active');
+  })
+  document.querySelectorAll('.contact-line').forEach(lineElement => {
+    lineElement.classList.add('contact-line_unactive');
+    lineElement.classList.remove('contact-line_active');
+  })
+}
+
+const changeListElement = (listElement) => {
+  if (listElement.classList.contains('contact-list-item_unactive')) {
+    listElement.classList.remove('contact-list-item_unactive');
+    listElement.classList.add('contact-list-item_active');
+    listElement.nextElementSibling.classList.remove('contact-line_unactive');
+    listElement.nextElementSibling.classList.add('contact-line_active');
+  }
+}
+
+const openCard = (card) => {
+  if (card.classList.contains('city-card_unactive')) {
+    card.classList.add('city-card_active');
+    card.classList.remove('city-card_unactive');
+  }
+}
+
+const closeAllCards = () => {
+  document.querySelectorAll('.city-card').forEach(card => {
+    card.classList.remove('city-card_active');
+    card.classList.add('city-card_unactive');
+  })
 }
